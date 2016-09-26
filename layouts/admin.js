d6cd7fb18272648903ff665016b5f01bc3593d1b
Harry Tang <harry@modernkernel.com>
2016-09-26 15:49:30 +0700

    update time format

--- layouts/admin.js
+++ layouts/admin.js
@@ -3,7 +3,7 @@ var timestamp = $(".server-time").data("timestamp");
 var datetime;
 function serverTime() {
     datetime = moment.unix(timestamp);
-    $(".server-time").html(moment().format('LLLL'));
+    $(".server-time").html(moment().format('L LTS'));
     timestamp += 1;
 }
 serverTime();
